ad4524c330e210ef74fbd453467fdfaa04486a92
sytnax52 <efehankalender5206@gmail.com>
2024-08-25 17:13:21 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,15 +3,15 @@ print("""Hesap Makinesine Hoşgeldiniz!""")
 IlkSayi=float(input("İlk Sayıyı Giriniz "))
 IkinciSayi = float(input("İkinci Sayıyı Giriniz "))
 islem=input("""Yapmak İstediğiniz İşlemi Giriniz: Toplama:+,Çıkarma:-,Çarpma:*,Bölme:/,İki Sayının Ortalamasını Alma:0""")
-if islem=="+":
+if islem == "+":
     print(str(IlkSayi+IkinciSayi))
-if islem=="-":
+if islem == "-":
     print(str(IlkSayi-IkinciSayi))
-if islem=="*":
+if islem == "*":
     print(str(IlkSayi*IkinciSayi))
-if islem=="/":
+if islem == "/":
     print(str(IlkSayi/IkinciSayi))
-if islem=="0":
+if islem == "0":
     print(str((IlkSayi+IkinciSayi)/2))
 while True:
     pass
